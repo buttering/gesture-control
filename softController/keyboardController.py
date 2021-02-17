b892ae0695bd3c7796d86dce1fc3f43d636adecd
buttering <54806742+buttering@users.noreply.github.com>
2021-02-17 18:23:10 +0800

    Design gestures

---
+++ softController/keyboardController.py
@@ -0,0 +1,72 @@
+from pynput import keyboard
+
+# 创建键盘实例
+kb = keyboard.Controller()
+
+
+def main():
+    # 创建监听
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release
+    )as listener:
+        listener.join()
+
+
+def on_press(key):
+    if key == keyboard.KeyCode.from_char('s'):
+        pagedownCtl()
+    print('{0} 被按下'.format(key))
+
+
+def on_release(key):
+    print('{0} 被释放'.format(key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+
+def pagedownCtl():
+    with kb.pressed(
+            keyboard.Key.page_down
+    ):
+        pass
+
+
+def pageupCtl():
+    with kb.pressed(
+            keyboard.Key.page_down
+    ):
+        pass
+
+
+def upCtl():
+    with kb.pressed(
+            keyboard.Key.page_down
+    ):
+        pass
+
+
+def downCtl():
+    with kb.pressed(
+            keyboard.Key.page_down
+    ):
+        pass
+
+
+def leftCtl():
+    with kb.pressed(
+            keyboard.Key.page_down
+    ):
+        pass
+
+
+def rightCtl():
+    with kb.pressed(
+            keyboard.Key.page_down
+    ):
+        pass
+
+
+if __name__ == '__main__':
+    main()
