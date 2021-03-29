9131150d177a2eee9062e1e773b432ed584b2b3f
chikangxi <chikangxi@hotmail.com>
2021-03-29 14:00:41 +0800

    gui development

--- gui/main.py
+++ gui/main.py
@@ -1,6 +1,8 @@
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QThread
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 import os
 import sys
@@ -9,12 +11,58 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir)))
 from manager import *
 
+# 状态显示条
+class StatusBar(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setMinimumWidth(300)
+
+        # 设置这个标志可使窗口总是显示在前台
+        # self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+        self.setWindowFlag(Qt.WindowStaysOnTopHint)
+        #self.setWindowFlag(Qt.FramelessWindowHint)
+
+        layout = QVBoxLayout(self)
+        self.setLayout(layout)
+
+        leftLabel = QLabel()
+        leftPixmap = QPixmap("gui/left.png")
+        leftLabel.setPixmap(leftPixmap)
+        layout.addWidget(leftLabel, 1)
+        rightPixmap = QPixmap("gui/right.png")
+        rightLabel = QLabel()
+        rightLabel.setPixmap(rightPixmap)
+        layout.addWidget(rightLabel, 1)
+
+        clockLabel = QLabel()
+        clockPixmap = QPixmap("gui/clock_rotate.png")
+        clockLabel.setPixmap(clockPixmap)
+        layout.addWidget(clockLabel, 1)
+
+# 会议界面
 class MeetingWindow(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        label = QLabel("已加入会议")
-        layout = QVBoxLayout(self)
-        layout.addWidget(label)
+
+        layout = QHBoxLayout(self)
+        vLayout1 = QVBoxLayout()
+        vLayout2 = QVBoxLayout()
+        layout.addLayout(vLayout1,3)
+        layout.addLayout(vLayout2,1)
+        self.setLayout(layout)
+        vLayout1.addWidget(QLabel("aaa"))
+        vLayout2.addWidget(QLabel("list"))
+        vLayout2.addWidget(QLabel("quit"))
+        hLayout1 = QHBoxLayout()
+        vLayout1.addLayout(hLayout1)
+        hLayout1.addWidget(QPushButton())
+        hLayout1.addWidget(QPushButton())
+        self.statusBar = StatusBar()
+        self.statusBar.show()
+
+
+
+
 
 class CreateMeetingThread(QThread):
     def __init__(self,manager):
@@ -48,16 +96,18 @@ class InitWidget(QWidget):
         self.setLayout(layout)
     
     def joinMeeting(self):
-        self.mainWindow.manager.joinMeeting(self.lineEdit.text())
+        #self.mainWindow.manager.joinMeeting(self.lineEdit.text())
         self.mainWindow.setCentralWidget(MeetingWindow())
     def createMeeting(self):
-        self.thread = CreateMeetingThread(self.mainWindow.manager)
-        self.thread.start()
+        #self.thread = CreateMeetingThread(self.mainWindow.manager)
+        #self.thread.start()
+        self.mainWindow.setCentralWidget(MeetingWindow())
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.resize(600,500)
+        self.setMinimumSize(600,500)
         self.manager = Manager()
         initWidget = InitWidget(self)
         self.setCentralWidget(initWidget)
