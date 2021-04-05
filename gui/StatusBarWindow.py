ca449c8f177595c2122833a7d4817defccc4172d
chikangxi <chikangxi@hotmail.com>
2021-04-06 00:17:14 +0800

    some change

--- gui/StatusBarWindow.py
+++ gui/StatusBarWindow.py
@@ -4,6 +4,12 @@ from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
+
+# 在引入父目录的模块之前加上如下代码：
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir)))
+
 from server.SocketServer import *
 import config.socketConfig as sc
 import threading
@@ -22,34 +28,75 @@ class StatusBarWindow(QWidget):
 
         self.setWindowFlag(Qt.FramelessWindowHint)
 
-        self.setWindowOpacity(0.5)
+        self.setWindowOpacity(0.9)
+
+        desktop = QApplication.desktop()
+        rect = desktop.geometry()
+        self.setGeometry(rect.width()-170,40,130,0)
 
         layout = QVBoxLayout(self)
         self.setLayout(layout)
 
         leftLabel = QLabel()
-        leftPixmap = QPixmap("gui/picture/left.png")
+        self.leftLabel = leftLabel
+        leftPixmap = QPixmap("gui/picture/左移.png")
         leftLabel.setPixmap(leftPixmap)
         layout.addWidget(leftLabel, 1)
-        rightPixmap = QPixmap("gui/picture/right.png")
+        rightPixmap = QPixmap("gui/picture/右移.png")
         rightLabel = QLabel()
+        self.rightLabel = rightLabel
         rightLabel.setPixmap(rightPixmap)
         layout.addWidget(rightLabel, 1)
 
         clockLabel = QLabel()
-        clockPixmap = QPixmap("gui/picture/clock_rotate.png")
+        self.clockLabel = clockLabel
+        clockPixmap = QPixmap("gui/picture/顺时针旋转.png")
         clockLabel.setPixmap(clockPixmap)
         layout.addWidget(clockLabel, 1)
 
+        anticlockLabel = QLabel()
+        self.anticlockLabel = anticlockLabel
+        anticlockPixmap = QPixmap("gui/picture/逆时针旋转.png")
+        anticlockLabel.setPixmap(anticlockPixmap)
+        layout.addWidget(anticlockLabel)
+
+        graspLabel = QLabel()
+        self.graspLabel = graspLabel
+        graspPixmap = QPixmap("gui/picture/抓取.png")
+        graspLabel.setPixmap(graspPixmap)
+        layout.addWidget(graspLabel)
+
+        zoominLabel = QLabel()
+        self.zoominlabel = zoominLabel
+        zoominPixmap = QPixmap("gui/picture/放大.png")
+        zoominLabel.setPixmap(zoominPixmap)
+        layout.addWidget(zoominLabel)
+
+        zoomoutLabel = QLabel()
+        self.zoomoutLabel = zoomoutLabel
+        zoomoutPixmap = QPixmap("gui/picture/缩小.png")
+        zoomoutLabel.setPixmap(zoomoutPixmap)
+        layout.addWidget(zoomoutLabel)
+
+    
+class GestureListener(object):
+    def __init__(self, statusBarWindow):
+        self.statusBarWindow = statusBarWindow
+    
+    def gestureChanged(self, gesturename):
+        print('gestureChanged',gesturename)
+
 if __name__ == '__main__':
-    def socket_server_start():
-        socket_server = SocketServer()
-        # socket_server.runServer(sc.IP, sc.PORT)
-        socket_server.runServer()
-    thread = threading.Thread(target=socket_server_start)
-    thread.start()
+
     app = QApplication(sys.argv)
     win = StatusBarWindow()
+    def socket_server_start():
+        gestureListener = GestureListener(win)
+        socket_server = SocketServer()
+        # socket_server.runServer(sc.IP, sc.PORT)
+        socket_server.runServer(gestureListener)
+    thread = threading.Thread(target=socket_server_start)
+    thread.start()
     win.show()
     app.exec_()
 
--- server/gestureController.py
+++ server/gestureController.py
@@ -17,6 +17,7 @@ class gestureController:
     # 为不同手势指定不同操作
     def callOperation(self, gesture: str) -> bool:
         # TODO:身份鉴权，只执行获取了焦点的操控者指令
+        self.__listener.gestureChanged(gesture)
         if gesture == "like":
             # TODO：点赞效果
             pass
